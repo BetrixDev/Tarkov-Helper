a2513facc557bc0d536180713b392718b985742e
BetrixEdits <53540292+BetrixEdits@users.noreply.github.com>
2021-01-09 21:59:12 -0500

    Add !Bitcoinfarm command

--- commands/bitcoinfarm.js
+++ commands/bitcoinfarm.js
@@ -11,19 +11,27 @@ module.exports = {
             return Price;
         }
         let PriceValue = await getPrice();
-        const BitcoinPriceTarkov =  Math.floor((PriceValue.data.amount * 0.19));
+        const BitcoinPriceTarkov =  Math.floor((PriceValue.data.amount * 0.19)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
         const SelectedGPUS = args[0];
-        if (SelectedGPUS > 50 || isNaN(SelectedGPUS) === true)
+        const BTCPerDay = (24 / Math.pow((0.04137931 - 0) + ((SelectedGPUS - 1) - 1) / ( 49 - 0)* (0.10386397 - 0), -1));
+        if (isNaN(SelectedGPUS) === true)
         {
             const ErrorMessage = new Discord.MessageEmbed()
             .setColor('RED')
             .setAuthor('Tarkov Helper', 'https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50SmallText.png?token=AMYPLRE73XI3MEKDQDCTJX277JKCK')
             .setTitle('Error!')
-            .setThumbnail('https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/ErrorLogo200x200.png?token=AMYPLRHITJTAM63PDKRMBX277JUYC')
-            .addFields(
-                {name: 'Error 1:', value: 'The input was not a number'},
-                {name: 'Error 2:' , value: 'The input was over the maximum amount of GPUS possible in a Bitcoin Farm (50)'}
-            )
+            .setDescription('The input was not a number')
+            .setThumbnail('https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/ErrorLogo128x128.png?token=AMYPLRFFVREOV35APX7JGJC77JVEU')
+            message.channel.send(ErrorMessage);
+        }
+        else if (SelectedGPUS > 50)
+        {
+            const ErrorMessage = new Discord.MessageEmbed()
+            .setColor('RED')
+            .setAuthor('Tarkov Helper', 'https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50SmallText.png?token=AMYPLRE73XI3MEKDQDCTJX277JKCK')
+            .setTitle('Error!')
+            .setDescription('The input was over the maximum amount of GPUS possible in a Bitcoin Farm (50)')
+            .setThumbnail('https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/ErrorLogo128x128.png?token=AMYPLRFFVREOV35APX7JGJC77JVEU')
             message.channel.send(ErrorMessage);
         }
         else
@@ -34,8 +42,9 @@ module.exports = {
             .setTitle('Bitcoin Farm Calculator')
             .setThumbnail('https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/BitcoinFarmLogo128x128.png?token=AMYPLRCWRRTB6UWB7CV7JKC77JS66')
             .addFields(
-                {name: 'Bitcoin Price:', value: `₽${BitcoinPriceTarkov.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`},
-                {name: 'Amount of GPUS:' , value: `${SelectedGPUS}`}
+                {name: 'Bitcoin Price:', value: `₽${BitcoinPriceTarkov}`},
+                {name: 'Amount of GPUS:' , value: `${SelectedGPUS}`},
+                {name: 'Bitcoins Per Day:', value: `${BTCPerDay}`}
             )
             message.channel.send(newEmbed);
         }
