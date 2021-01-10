e6d1a8a102da67bf6b1a2d446dc58620357599df
BetrixEdits <53540292+BetrixEdits@users.noreply.github.com>
2021-01-09 20:22:08 -0500

    Added Assets

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'help',
-    description: "Get help with how to use commands",
+    description: "Get help with how to use commands. Sytax: !COMMAND {CUSTOM USER INPUT FIELD}",
     execute(message, args, Discord){
         const newEmbed = new Discord.MessageEmbed()
-        .setAuthor('Tarkov Helper', 'https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50.png?token=AMYPLRG4G6UW3AQMC5BR7QC77JJ56')
+        .setAuthor('Tarkov Helper', 'https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50SmallText.png?token=AMYPLRE73XI3MEKDQDCTJX277JKCK')
         .setTitle('Help')
         .setDescription('All of the commands currently supported by Tarkov Helper')
         .addFields(
