5cf187e3ef274f8d8bc62448488df7f3d17b42c1
BetrixEdits <betrix303@gmail.com>
2021-01-13 06:04:47 -0500

    Update map.js
    Got rid of useless variables and cleaned up message handler

--- commands/map.js
+++ commands/map.js
@@ -1,14 +1,10 @@
 const { ErrorMessage } = require("../command_modules/errormessage")
 const { MapMessage } = require("../command_modules/mapmessage")
 
-const Thumbnail = "https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/MapLogo128x128.png"
-const AuthorImage = "https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50SmallText.png?token=AMYPLRE73XI3MEKDQDCTJX277JKCK"
-const FooterText = "Map image taken from the EFT WIKI"
-
 module.exports = {
     name: 'map',
     description: 'Returns an image of the map specified',
-    execute(message, args, Discord) {
+    execute(message, args) {
         switch (args[0]) {
             case 'customs':
             case 'c':
@@ -34,39 +30,13 @@ module.exports = {
                 switch (args[1]) {
                     case 'extractions':
                     case 'e':
-                        const newEmbed = new Discord.MessageEmbed()
-                            .setColor('#cecdc3')
-                            .setAuthor('Tarkov Helper', AuthorImage)
-                            .setTitle('Reserve Extractions')
-                            .setThumbnail(Thumbnail)
-                            .addFields({ name: 'For the full-res image click below', value: '[Full Resolution Image](https://escapefromtarkov.gamepedia.com/File:3D_Map_by_loweffortsaltbox.png)' }, )
-                            .setImage('https://github.com/BetrixEdits/Tarkov-Helper/blob/master/Assets/Maps/Reserve/Extractions.png?raw=true')
-                            .setFooter(FooterText)
-                        message.channel.send(newEmbed);
-                        break;
+                        MapMessage('Reserve Extractions', 'https://github.com/BetrixEdits/Tarkov-Helper/blob/master/Assets/Maps/Reserve/Extractions.png?raw=true', 'https://escapefromtarkov.gamepedia.com/File:3D_Map_by_loweffortsaltbox.png', message)
                     case 'keys':
                     case 'k':
-                        const newEmbed2 = new Discord.MessageEmbed()
-                            .setColor('#cecdc3')
-                            .setAuthor('Tarkov Helper', AuthorImage)
-                            .setTitle('Reserve Keys')
-                            .setThumbnail(Thumbnail)
-                            .addFields({ name: 'For the full-res image click below', value: '[Full Resolution Image](https://escapefromtarkov.gamepedia.com/File:Keys_and_doors_v3.png)' }, )
-                            .setImage('https://github.com/BetrixEdits/Tarkov-Helper/blob/master/Assets/Maps/Reserve/Keys.png?raw=true')
-                            .setFooter(FooterText)
-                        message.channel.send(newEmbed2);
+                        MapMessage('Reserve Keys', 'https://github.com/BetrixEdits/Tarkov-Helper/blob/master/Assets/Maps/Reserve/Keys.png?raw=true', 'https://escapefromtarkov.gamepedia.com/File:Keys_and_doors_v3.png', message)
                         break;
                     case undefined:
-                        const newEmbed3 = new Discord.MessageEmbed()
-                            .setColor('#cecdc3')
-                            .setAuthor('Tarkov Helper', AuthorImage)
-                            .setTitle('Reserve Default Map')
-                            .setThumbnail(Thumbnail)
-                            .addFields({ name: 'For the full-res image click below', value: '[Full Resolution Image](https://escapefromtarkov.gamepedia.com/File:Reserve_Map_Translated.png)' }, )
-                            .setDescription('Customs Default \nFor more maps of Customs type: \n!map customs {extractions, keys}')
-                            .setImage('https://github.com/BetrixEdits/Tarkov-Helper/blob/master/Assets/Maps/Reserve/Default.png?raw=true')
-                            .setFooter(FooterText)
-                        message.channel.send(newEmbed3);
+                        MapMessage('Reserve Default Map', 'https://github.com/BetrixEdits/Tarkov-Helper/blob/master/Assets/Maps/Reserve/Default.png?raw=true', 'https://escapefromtarkov.gamepedia.com/File:Reserve_Map_Translated.png', message)
                         break;
                     default:
                         ErrorMessage('The general map was found, but the specific map was not', message)
@@ -77,7 +47,6 @@ module.exports = {
             case 'i':
                 switch (args[1]) {
                     case 'extractions':
-
                         break;
                     default:
                         ErrorMessage('The general map was found, but the specific map was not', message)
