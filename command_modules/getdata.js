a976adbd820b43c3dc01dde5c1ef5b56dd30622c
BetrixEdits <betrix303@gmail.com>
2021-01-18 03:15:42 -0500

    Added Item Name Filter
    Since the name of the JSON object is what will be used for searches, filtering out useless recurring words will make searching easier

--- command_modules/getdata.js
+++ command_modules/getdata.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-
+var Filter = require('../game_data/itemfilter.json')
 
 const GetItems = () => {
     fs.writeFileSync('./game_data/itemnames.json', JSON.stringify({}))
@@ -9,11 +9,18 @@ const GetItems = () => {
         for (const ItemID in JSONData) {
             let name = JSONData[ItemID]._name
             let id = JSONData[ItemID]._id
+            let formattedname = name.split("_").join(" ")
+            for (const FilterItem in Filter) {
+                let TestItem = Filter[FilterItem]
+                if (formattedname.includes(TestItem)) {
+                    formattedname = formattedname.split(Filter[FilterItem]).join("")
+                }
+            }
             let NewJsonEntry = {
                 Name: name,
                 ID: id
             }
-            NewJSON[name] = NewJsonEntry
+            NewJSON[formattedname] = NewJsonEntry
         }
         fs.writeFileSync('./game_data/itemnames.json', JSON.stringify(NewJSON, null, 2))
     })
