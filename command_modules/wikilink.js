aa4441a53b7fadba0fe3b89bd01458c34c8dd202
BetrixEdits <betrix303@gmail.com>
2021-01-21 07:42:11 -0500

    Minor Fixes

--- command_modules/wikilink.js
+++ command_modules/wikilink.js
@@ -1,7 +1,7 @@
 const urlExist = require('url-exist');
 
 const GetWikiLink = async(Item) => {
-    let WikiLink = `https://escapefromtarkov.gamepedia.com/${Item.replace(' ', '_')}`
+    let WikiLink = `https://escapefromtarkov.gamepedia.com/${Item.replace('. ', '_-_').replace(' ', '_').split(' ').join('_')}`
     if (await urlExist(WikiLink)) {
         console.log(WikiLink)
         return WikiLink
