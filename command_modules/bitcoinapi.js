11ea1b3478e061dfdc4f894f72f7f4ab77ee0923
BetrixEdits <53540292+BetrixEdits@users.noreply.github.com>
2021-02-05 12:05:46 -0500

    Update bitcoinapi.js

--- command_modules/bitcoinapi.js
+++ command_modules/bitcoinapi.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const GetBitcoinPrice = async() => {
     let getPrice = async() => {
+        // Using Coinbase API until I get an actual price API for tarkov
         let response = await axios.get('https://api.coinbase.com/v2/prices/spot?currency=RUB');
         let Price = response.data;
         return Price;
