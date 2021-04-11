da5166ba16340cd2ed5e3cad2f1146a5f696e6ef
BetrixEdits <betrix303@gmail.com>
2021-04-10 22:26:01 -0400

    Tarkov Helper Rewrite
    Rewriting to clean up code aswell as use the new slash command api from Discord

--- commands/bitcoinfarm.js
+++ commands/bitcoinfarm.js
@@ -1,51 +1,56 @@
-const { GetBitcoinPrice } = require('../command_modules/bitcoinapi');
-const { ErrorMessage } = require('../command_modules/errormessage');
+const Settings = require('../settings.json')
 
-module.exports = {
-    name: 'bitcoinfarm',
-    description: "Get help with how to use commands. Sytax: !COMMAND {CUSTOM USER INPUT FIELD}",
-    async execute(message, args, Discord) {
-        const PriceValue = await GetBitcoinPrice()
-        if (args[0] === 'c' || args[0] === 'compare') {
-            let BitcoinPriceTarkovFormat = Math.floor((PriceValue.data.amount * 0.19)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-            let SelectedGPUS = args[1]
-            let BTCPerDay = (24 / Math.pow((0.04137931 - 0) + ((SelectedGPUS - 1) - 1) / (49 - 0) * (0.10386397 - 0), -1));
-            let SelectedGPUSComp = args[2]
-            let BTCPerDayComp = (24 / Math.pow((0.04137931 - 0) + ((SelectedGPUSComp - 1) - 1) / (49 - 0) * (0.10386397 - 0), -1));
-            if (isNaN(SelectedGPUS) === true || isNaN(SelectedGPUSComp) === true) {
-                ErrorMessage('The input was not a number', message)
-            } else if (SelectedGPUS > 50 || SelectedGPUSComp > 50) {
-                ErrorMessage('The input was over the maximum amount of GPUS possible in a Bitcoin Farm (50)', message)
-            } else {
-                const newEmbed = new Discord.MessageEmbed()
-                    .setColor('#cecdc3')
-                    .setAuthor('Tarkov Helper', 'https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50SmallText.png?token=AMYPLRE73XI3MEKDQDCTJX277JKCK')
-                    .setTitle('Bitcoin Farm Calculator')
-                    .setThumbnail('https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/BitcoinFarmLogo128x128.png?token=AMYPLRCWRRTB6UWB7CV7JKC77JS66')
-                    .setDescription('Compare Mode')
-                    .addFields({ name: 'Bitcoin Price:', value: `₽${BitcoinPriceTarkovFormat}` }, { name: 'Difference in GPUS:', value: `${(SelectedGPUSComp - SelectedGPUS)}` }, { name: 'Difference in Bitcoins Per Day:', value: `${(BTCPerDayComp - BTCPerDay)}` }, { name: 'Difference in Roubles Per Day:', value: `₽${(Math.floor((BTCPerDayComp * Math.floor((PriceValue.data.amount * 0.19)))) - Math.floor((BTCPerDay * Math.floor((PriceValue.data.amount * 0.19))))).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}` })
-                message.channel.send(newEmbed);
-            }
-        } else // Default mode
-        {
-            const BitcoinPriceTarkovFormat = Math.floor((PriceValue.data.amount * 0.19)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-            const SelectedGPUS = args[0];
-            // Equation provide by the Escape From Tarkov Wiki
-            const BTCPerDay = (24 / Math.pow((0.04137931 - 0) + ((SelectedGPUS - 1) - 1) / (49 - 0) * (0.10386397 - 0), -1));
-            const RUBPerDay = Math.floor((BTCPerDay * Math.floor((PriceValue.data.amount * 0.19)))).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-            if (isNaN(SelectedGPUS) === true) {
-                ErrorMessage('The input was not a number', message)
-            } else if (SelectedGPUS > 50) {
-                ErrorMessage('The input was over the maximum amount of GPUS possible in a Bitcoin Farm (50)', message)
-            } else {
-                const newEmbed = new Discord.MessageEmbed()
-                    .setColor('#cecdc3')
-                    .setAuthor('Tarkov Helper', 'https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/Logo50x50SmallText.png?token=AMYPLRE73XI3MEKDQDCTJX277JKCK')
-                    .setTitle('Bitcoin Farm Calculator')
-                    .setThumbnail('https://raw.githubusercontent.com/BetrixEdits/Tarkov-Helper/master/Assets/Media/BitcoinFarmLogo128x128.png?token=AMYPLRCWRRTB6UWB7CV7JKC77JS66')
-                    .addFields({ name: 'Bitcoin Price:', value: `₽${BitcoinPriceTarkovFormat}` }, { name: 'Amount of GPUS:', value: `${SelectedGPUS}` }, { name: 'Bitcoins Per Day:', value: `${BTCPerDay}` }, { name: 'Roubles Per Day:', value: `₽${RUBPerDay}` })
-                message.channel.send(newEmbed);
-            }
-        }
+class BitcoinFarmCalc {
+    constructor(amount) {
+        this.BTCPerDay = (1 / ((110000 / (1 + (amount - 1) * 0.044225)) / 3600)) * 24
+        this.RUBPerDay = this.BTCPerDay * 650000
     }
 }
+
+const bitcoinfarm = (DiscordJS, args) => {
+    const Calculation = new BitcoinFarmCalc(args['gpus'])
+
+    if (args['compare-gpus'] === undefined) {
+        const Embed = new DiscordJS.MessageEmbed()
+            .setColor(Settings.BotSettings['Color'])
+            .setTitle('Bitcoin Farm Calculator')
+            .setThumbnail(Settings.Images.Thumbnails['BitcoinFarm'])
+            .addFields({
+                name: "Bitcoin Price:",
+                value: '₽650,000'
+            }, {
+                name: "Amount of GPUS:",
+                value: args['gpus']
+            }, {
+                name: "Bitcoins Per Day:",
+                value: Calculation.BTCPerDay
+            }, {
+                name: "Roubles Per Day:",
+                value: `₽${Math.round(Calculation.RUBPerDay).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+            })
+        return Embed
+    } else {
+        const SecondCalculation = new BitcoinFarmCalc(args['compare-gpus'])
+
+        const Embed = new DiscordJS.MessageEmbed()
+            .setColor(Settings.BotSettings.Color)
+            .setTitle('Bitcoin Farm Calculator')
+            .setThumbnail(Settings.Images.Thumbnails['BitcoinFarm'])
+            .addFields({
+                name: "Bitcoin Price:",
+                value: '₽650,000'
+            }, {
+                name: "Difference in GPUS:",
+                value: (Math.abs(args['gpus'] - args['compare-gpus']))
+            }, {
+                name: "Difference in Bitcoins Per Day:",
+                value: Math.abs(Calculation.BTCPerDay - SecondCalculation.BTCPerDay)
+            }, {
+                name: "Difference in Roubles Per Day:",
+                value: `₽${Math.abs((Math.round(Calculation.RUBPerDay) - Math.round(SecondCalculation.RUBPerDay))).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}`
+            })
+        return Embed
+    }
+}
+
+exports.bitcoinfarm = bitcoinfarm
--- main.js
+++ main.js
@@ -1,108 +1,99 @@
-let Start = new Date()
+const DiscordJS = require('discord.js')
+const fs = require('fs')
+require('dotenv').config()
 
-// Load required modules
-var Discord = require('discord.js');
-var client = new Discord.Client();
-var cron = require('node-cron')
-var fs = require('fs');
-require('dotenv').config();
+const guildID = '797601083589001227'
+const client = new DiscordJS.Client()
 
+const getApp = (guildID) => {
+    const app = client.api.applications(client.user.id)
+    if (guildID) {
+        app.guilds(guildID)
+    }
+    return app
+}
 
-// load command_modules
-//var { GetConfigData } = require("./command_modules/getconfigdata")
-var { GetCalibers } = require("./command_modules/getcalibers");
-var { NotifyMessage } = require("./command_modules/notifymessage")
+const BotCommands = new Array()
+const CommandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+for (const File of CommandFiles) {
+    BotCommands.push(File.split('.')[0])
+}
 
-// Load data
-let ResetData = JSON.parse(fs.readFileSync('./resetdata.json'))
+client.on('ready', async() => {
+    console.log('Bot Ready')
 
+    const commands = await getApp(guildID).commands.get()
+        //console.log(commands)
 
-// Load all commands
-client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
+    await getApp(guildID).commands.post({
+        data: {
+            name: 'bitcoinfarm',
+            description: 'Calculate to amount of money obtained from a certain amount of Graphics Cards',
+            options: [{
+                    name: 'gpus',
+                    description: 'Amount of GPUS to calculate with',
+                    required: true,
+                    type: 4
+                },
+                {
+                    name: "compare-gpus",
+                    description: 'Use this to display the difference between two different GPU amount',
+                    required: false,
+                    type: 4
+                }
+            ]
+        }
+    })
 
-    client.commands.set(command.name, command);
+    client.ws.on('INTERACTION_CREATE', async(interaction) => {
+        const { name, options } = interaction.data
+        const command = name.toLowerCase()
+        const args = {}
+
+        if (options) {
+            for (const option of options) {
+                const { name, value } = option
+                args[name] = value
+            }
+        }
+
+        // Commands return an embeded message
+
+        // If command exists locally
+        if (BotCommands.includes(command)) {
+            const message = require(`./commands/${command}`)[command](DiscordJS, args)
+            Reply(interaction, message)
+        } else {
+            Reply(interaction, 'This command has no logic yet')
+        }
+    })
+})
+
+const Reply = async(interaction, response) => {
+    let data = {
+        content: response
+    }
+    if (typeof response === 'object') {
+        data = await CreateAPIMessage(interaction, response)
+    }
+    client.api.interactions(interaction.id, interaction.token).callback.post({
+        data: {
+            type: 4,
+            data
+        }
+    })
+}
+
+const CreateAPIMessage = async(interaction, content) => {
+    const { data, files } = await DiscordJS.APIMessage.create(
+            client.channels.resolve(interaction.channel_id),
+            content
+        )
+        .resolveData()
+        .resolveFiles()
+
+    return {...data, files }
 }
 
 
-// Call function to load calibers
-GetCalibers()
-
-
-client.once('ready', () => {
-    let End = new Date()
-    console.log(`Tarkov Helper Initialized in ${End.getTime() - Start.getTime()}ms`)
-})
-
-
-// Command handler
-client.on('message', async message => {
-    let prefix = "!th"
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).split(/ +/);
-    // Fix for having a space between prefix and command just quality of life change
-    if (args[0] === '') { args.shift() }
-
-    const command = args.shift().toLowerCase();
-
-    /*
-    // Using if statement since has multiple conditions
-    if (command === "notify" && message.guild === null) {
-        client.commands.get('notify').execute(message, args, Discord);
-    }
-    */
-
-    switch (command) {
-        case 'help':
-        case 'h':
-            client.commands.get('help').execute(message, args, Discord);
-            break;
-        case 'bitcoinfarm':
-        case 'bf':
-            client.commands.get('bitcoinfarm').execute(message, args, Discord);
-            break;
-        case 'map':
-        case 'm':
-            client.commands.get('map').execute(message, args, Discord);
-            break;
-            /*
-        case 'price':
-        case 'p':
-            client.commands.get('price').execute(message, args, Discord);
-            break;
-            */
-        case 'prefixset':
-            client.commands.get('prefix').execute(message, args, Discord);
-            break;
-        case 'stats':
-        case 's':
-            client.commands.get('stat').execute(message, args, Discord);
-            break;
-        case 'xpto':
-        case 'xp':
-            client.commands.get('xpto').execute(message, args, Discord);
-            break;
-        case 'quest':
-        case 'q':
-            client.commands.get('quest').execute(message, args, Discord);
-            break;
-        case 'caliber':
-        case 'calibre':
-        case 'c':
-            client.commands.get('caliber').execute(message, args, Discord);
-            break;
-            /*
-        case 'weaponbuilder':
-        case 'weaponbuild':
-        case 'wb':
-            client.commands.get('weaponbuilder').execute(message, args, Discord);
-            break;
-            */
-    }
-});
-
-// Log to bot in with custom bot token
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN)
