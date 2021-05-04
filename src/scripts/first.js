fafbc475314f4ffa786fad354799b728f90dc99b
BetrixEdits <betrix303@gmail.com>
2021-05-04 18:19:58 -0400

    Update first.js

--- src/scripts/first.js
+++ src/scripts/first.js
@@ -1,4 +1,4 @@
 // This file will retrieve game data and is run in isolation for the first run so
 // that it can load the data before any module or command is run
-const { StartTasks } = require('./tasks')
+const { StartTasks } = require('../tasks')
 StartTasks()
