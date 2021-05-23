de6f596cc82e67afb2f5bce83d42414622369b13
BetrixEdits <betrix303@gmail.com>
2021-05-22 22:58:09 -0400

    Added Caliber Command

---
+++ src/command_modules/calibersearchengine.js
@@ -0,0 +1,45 @@
+const AccurateSearch = require('accurate-search')
+const fs = require('fs')
+
+const ItemData = JSON.parse(fs.readFileSync('./src/game_data/api/itemdata.json'))
+
+let SearchEngine = new AccurateSearch()
+
+let CaliberData = {}
+let Calibers = new Array()
+
+function InitEngine() {
+    for (const i in ItemData) {
+        let Item = ItemData[i]
+
+        if (Item.Types.includes('ammo') && !Item.Types.includes('grenade') && !Item.Name.includes('pcs.')) {
+            let Caliber = Item.Name.split(' ')[0].replace('mm', '') // Gives better result then using 'RawData.Caliber'
+
+            if (CaliberData[Caliber] === undefined) {
+                SearchEngine.addText(Caliber, Caliber)
+                Calibers.push(Caliber)
+                SearchEngine.addText(Caliber, Caliber.replace('.', ''))
+                CaliberData[Caliber] = new Array()
+            }
+
+            let RawData = Item.RawData.Data
+
+            CaliberData[Caliber].push({
+                Name: Item.ShortName,
+                ID: Item.ID,
+                Damage: RawData.Damage,
+                ArmorDamage: RawData.ArmorDamage,
+                Penetration: RawData.PenetrationPower
+            })
+        }
+    }
+}
+
+const Engine = (Input) => {
+    let Results = SearchEngine.search(Input.replace(' ', 'x'))
+
+    return { Results: Results, CaliberData: CaliberData }
+}
+
+exports.InitCaliberEngine = InitEngine
+exports.CaliberSearchEngine = Engine
---
+++ src/commands/caliber.js
@@ -0,0 +1,88 @@
+// Command Config
+const CommandSettings = {
+    CommandData: {
+        data: {
+            name: 'caliber',
+            description: 'Returns basic info on how to use Tarkov Helper',
+            options: [{
+                name: 'caliber',
+                description: 'What caliber to list information of',
+                required: true,
+                type: 3
+            }]
+        }
+    },
+    DMCommand: true
+}
+
+const { CaliberSearchEngine } = require('../command_modules/calibersearchengine')
+const { MessageEmbed } = require('discord.js')
+
+// Command Functions
+const CommandFunction = (args) => {
+    let Caliber = args['caliber']
+
+    let EngineResults = CaliberSearchEngine(Caliber)
+    Caliber = EngineResults.Results[0]
+
+    let CaliberData = EngineResults.CaliberData[Caliber]
+
+    let NonSortedArray = new Array()
+
+    for (const Ammo of CaliberData) {
+        NonSortedArray.push({ Name: Ammo.Name, Damage: Ammo.Damage, Penetration: Ammo.Penetration })
+    }
+
+    let SortedValues = SortValues(NonSortedArray, 1)
+
+    let NameString = `${SortedValues[0].join('\n')}`
+    let DamageString = `${SortedValues[1].join('\n')}`
+    let PenetrationString = `${SortedValues[2].join('\n')}`
+
+    let Message = new MessageEmbed()
+        .setTitle(`${Caliber} Data`)
+        .setDescription('For a list of all possible calibers [Click Here](https://gist.github.com/BetrixEdits/16c20db88feb4aefd22dbac6e257e290)')
+        .addFields({
+            name: 'Name',
+            value: NameString,
+            inline: true
+        }, {
+            name: 'Damage',
+            value: DamageString,
+            inline: true
+        }, {
+            name: 'Penetration',
+            value: PenetrationString,
+            inline: true
+        })
+
+    return { Type: "ServerMessage", Content: Message }
+}
+
+function SortValues(Values, Type) {
+
+    const Types = ['Damage', 'Penetration']
+    let InputArray = Values
+
+    InputArray.sort(function(a, b) {
+        return b[Types[Type]] - a[Types[Type]]
+    })
+
+    let OutputNames = new Array()
+    let OutputDamages = new Array()
+    let OutputPenetrations = new Array()
+
+    for (const Ammo in InputArray) {
+
+        OutputNames.push(InputArray[Ammo].Name)
+        OutputDamages.push(InputArray[Ammo].Damage)
+        OutputPenetrations.push(InputArray[Ammo].Penetration)
+
+    }
+
+    return [OutputNames, OutputDamages, OutputPenetrations]
+
+}
+
+exports.CommandFunction = CommandFunction
+exports.CommandSettings = CommandSettings
