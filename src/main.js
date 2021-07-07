3144259547f7cd65a1c77020096ba71803527c71
BetrixEdits <betrix303@gmail.com>
2021-07-07 08:27:41 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // Tarkov Data API from Tarkov Tools https://tarkov-tools.com/about/
 // Some code snippets are also from Tarkov Tools
 async function StartBot(Dev) {
-
     // Cache data and start the scheduled functions for grabbing data from apis
+    require('./command_modules/itemsearchengine')
     require('./command_modules/searchengine').InitSearchEngine()
     require('./command_modules/calibersearchengine').InitCaliberEngine()
 
@@ -11,8 +11,7 @@ async function StartBot(Dev) {
         require('./tasks').StartTasks()
     }
 
-    require('./server').KeepAlive() // creates a server that can be pinged to keep the bot running for using hosting services like replit
     require('./bot').InitBot(Dev)
 
 }
-StartBot() // Pass in true to use BOT_TOKEN_DEV to log your bot in and bypass waiting for caching
+StartBot(true) // Pass in true to use BOT_TOKEN_DEV to log your bot in and bypass waiting for caching
