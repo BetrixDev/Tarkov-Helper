dba21ef4cc7167d56895ed6e8b0e049cb6039ef6
BetrixEdits <betrix303@gmail.com>
2021-07-07 22:43:59 -0400

    Update bitcoinfarmcalc.js

--- src/classes/bitcoinfarmcalc.js
+++ src/classes/bitcoinfarmcalc.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 class BitcoinFarmCalc {
     constructor(gpus) {
         this.ItemPriceData = JSON.parse(fs.readFileSync('./src/game_data/api/pricedata.json'))
-        this.BTCPrice = this.ItemPriceData['59faff1d86f7746c51718c9c'].Item.traderPrices[4].price
+        this.BTCPrice = this.ItemPriceData['59faff1d86f7746c51718c9c'].Item.traderPrices[3].price
         this.BTCPerDay = (1 / ((110000 / (1 + (gpus - 1) * 0.044225)) / 3600)) * 24
         this.RUBPerDay = this.BTCPerDay * this.BTCPrice
     }
