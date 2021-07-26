0c826991cb6157d35f20a1031cbbb8505ab23efd
BetrixEdits <betrix303@gmail.com>
2021-07-26 15:31:53 -0400

    Added a command tracker
    Tracks how many times each command is issued and stores it in a mongoDB collection

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const serverModel = require('./models/serverSchema')
-const fs = require('fs')
+const commandCounter = require('./models/commandSchema')
 
 const GetServerData = async(serverid) => {
     let serverData
@@ -33,6 +33,33 @@ const SetServerData = async(serverid, key, data) => {
     })
 }
 
+const IncreaseCommands = async(commandName) => {
+    let commandData
 
+    try {
+        commandData = await commandCounter.findOne({ name: commandName })
+
+        if (!commandData) {
+            let command = await commandCounter.create({
+                name: commandName,
+                commands: 1
+            })
+            await command.save()
+        } else {
+            await commandCounter.findOneAndUpdate({
+                name: commandName
+            }, {
+                $inc: {
+                    commands: 1
+                }
+            })
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+exports.IncreaseCommands = IncreaseCommands
 exports.GetServerData = GetServerData
 exports.SetServerData = SetServerData
