abf1794102560fba26f45bb95eee821f8c7e8c82
BetrixEdits <betrix303@gmail.com>
2021-07-27 20:55:02 -0400

    Fixed Title and Description for map command

--- src/commands/map.js
+++ src/commands/map.js
@@ -1,6 +1,8 @@
 require('../utils')
 const { MessageEmbed } = require('discord.js')
 const PossibleMaps = require('../game_data/maps.json')
+let LocationStrings = ReadJson('./src/game_data/database/locales/global/en.json').locations
+console.log(LocationStrings)
 
 function GetMaps() {
     let Result = new Array()
@@ -32,23 +34,37 @@ const CommandSettings = {
     }
 }
 
+let mapIDs = {
+    "laboratory": "5b0fc42d86f7744a585f9105",
+    "bigmap": "56f40101d2720b2a4d8b45d6",
+    "woods": "5704e3c2d2720bac5b8b4567",
+    "rezervbase": "5704e5fad2720bc05b8b4567",
+    "shoreline": "5704e554d2720bac5b8b456e",
+    "interchange": "5714dbc024597771384a510d",
+    "factory4_day": "55f2d3fd4bdc2d5f408b4567"
+}
+
 // Command Functions
 const CommandFunction = (args) => {
     let Map = args['map']
 
-    let MapData = require(`../game_data/database/locations/${Map}/base.json`)
-    let MapLootData = require(`../game_data/database/locations/${Map}/loot.json`)
+    let MapData = ReadJson(`./src/game_data/database/locations/${Map}/base.json`)
+    let MapLootData = ReadJson(`./src/game_data/database/locations/${Map}/loot.json`)
+
+    let mapID = mapIDs[Map]
+
+    let mapName = LocationStrings[mapID].Name
 
     return {
         Type: "ServerMessage",
         Content: new MessageEmbed()
             .setColor(Settings.BotSettings.Color)
-            .setTitle(`${CapitalizeName(Map)}`)
-            .setDescription(MapData.Description)
+            .setTitle(mapName)
+            .setDescription(LocationStrings[mapID].Description)
             .setThumbnail(`https://raw.githubusercontent.com/Tarkov-Helper/Image-Database/main/map_icons/${Map.replace(' ', '%20')}.png`)
             .addFields({
                 name: 'Map Genie',
-                value: `[Click Here](https://mapgenie.io/tarkov/maps/${Map.replace('the ','')})`,
+                value: `[Click Here](https://mapgenie.io/tarkov/maps/${mapName.replace('The ', '')})`,
                 inline: true
             }, {
                 name: 'Player Count',
