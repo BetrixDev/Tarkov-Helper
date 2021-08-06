573e9b13957132cc21c55eca0fba25792c2e9f4d
BetrixEdits <betrix303@gmail.com>
2021-08-06 02:02:54 -0400

    Delete messageuser.js

--- src/command_modules/messageuser.js
+++
@@ -1,9 +0,0 @@
-const MessageUser = (client, uid, msg) => {
-    client.users.fetch(uid).then((user) => {
-        user.send(msg).then((message) => {
-            return message
-        })
-    })
-}
-
-exports.MessageUser = MessageUser
