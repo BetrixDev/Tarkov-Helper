96b5114a9c28023563f7330c63e0fd37dceba93e
BetrixEdits <betrix303@gmail.com>
2021-12-29 16:04:19 -0500

    Paginated barter command

--- src/commands/barter.ts
+++ src/commands/barter.ts
@@ -1,6 +1,15 @@
 import 'reflect-metadata'
-import { Discord, Slash, SlashOption } from 'discordx'
-import { AutocompleteInteraction, CommandInteraction, EmbedFieldData, MessageEmbed } from 'discord.js'
+import { ButtonComponent, Discord, Slash, SlashOption } from 'discordx'
+import {
+    AutocompleteInteraction,
+    ButtonInteraction,
+    CommandInteraction,
+    EmbedFieldData,
+    InteractionReplyOptions,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed
+} from 'discord.js'
 import SearchEngine from '../helpers/search_engines/item-engine'
 import { ErrorReponse, FormatPrice, GetBarter, GetItem, isID, isShortName, ItemImage } from '../lib'
 import settings from '../data/settings'
@@ -47,10 +56,23 @@ export class BarterCommand {
         }
     }
 
-    message(id: string) {
+    @ButtonComponent(/^barter__/)
+    barterButton(interaction: ButtonInteraction) {
+        const [_, action, id, page] = interaction.customId.split('__')
+
+        if (action === 'back') {
+            interaction.update(this.message(id, Number(page) - 1))
+        } else {
+            interaction.update(this.message(id, Number(page) + 1))
+        }
+    }
+
+    message(id: string, page = 0): InteractionReplyOptions {
         const item = GetItem(id)
         const barterData = new BaterData(item.id)
 
+        const pages = Math.ceil(barterData.Barters.length / 9)
+        console.log(pages)
         if (barterData.Barters.length === 0) {
             return {
                 embeds: [
@@ -62,16 +84,36 @@ export class BarterCommand {
                 ]
             }
         }
-        return {
+        let msg: InteractionReplyOptions = {
             embeds: [
                 new MessageEmbed()
                     .setThumbnail(ItemImage(item.id))
                     .setColor(settings.botSettings.color)
                     .setTitle(`${item.shortName} Barters`)
                     .setDescription(`[Wiki Link](${item.wikiLink})`)
-                    .setFields(barterData.Barters.slice(0, 18))
+                    .setFields(barterData.Barters.slice(page * 9, page * 9 + 9))
+            ],
+            components: []
+        }
+
+        if (barterData.Barters.length > 9) {
+            msg.components = [
+                new MessageActionRow().addComponents(
+                    new MessageButton()
+                        .setCustomId(`barter__back__${id}__${page}`)
+                        .setLabel('Last Barter')
+                        .setDisabled(page === 0)
+                        .setStyle('PRIMARY'),
+                    new MessageButton()
+                        .setCustomId(`barter__forward__${id}__${page}`)
+                        .setLabel('Next Barter')
+                        .setDisabled(page === pages - 1)
+                        .setStyle('PRIMARY')
+                )
             ]
         }
+
+        return msg
     }
 }
 
--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -1,21 +1,35 @@
-import { CommandInteraction, InteractionReplyOptions, MessageEmbed } from 'discord.js'
+import { CommandInteraction, Interaction, InteractionReplyOptions, MessageEmbed } from 'discord.js'
 import settings from '../data/settings'
 
-export function ErrorReponse(message: string, interaction: CommandInteraction): InteractionReplyOptions {
-    const args = interaction.options.data.map((arg) => {
-        return `${arg.name}: ${arg.value}`
-    })
+export function ErrorReponse(message: string, interaction: Interaction): InteractionReplyOptions {
+    if (interaction.isCommand()) {
+        const args = interaction.options.data.map((arg) => {
+            return `${arg.name}: ${arg.value}`
+        })
 
-    return {
-        embeds: [
-            new MessageEmbed()
-                .setAuthor('Tarkov Helper')
-                .setColor(settings.botSettings.errorColor)
-                .setTitle('The command issued had and error')
-                .setDescription(`\`${message}\``)
-                .addField('Args', args.join('\n'))
-                .setFooter(`Command issued: ${interaction.commandName}`)
-        ],
-        ephemeral: true
+        return {
+            embeds: [
+                new MessageEmbed()
+                    .setAuthor('Tarkov Helper')
+                    .setColor(settings.botSettings.errorColor)
+                    .setTitle('The command issued had and error')
+                    .setDescription(`\`${message}\``)
+                    .addField('Args', args.join('\n'))
+                    .setFooter(`Command issued: ${interaction.commandName}`)
+            ],
+            ephemeral: true
+        }
+    } else {
+        return {
+            embeds: [
+                new MessageEmbed()
+                    .setAuthor('Tarkov Helper')
+                    .setColor(settings.botSettings.errorColor)
+                    .setTitle('The command issued had and error')
+                    .setDescription(`\`${message}\``)
+                    .setFooter(`Command issued: ${interaction.id}`)
+            ],
+            ephemeral: true
+        }
     }
 }
