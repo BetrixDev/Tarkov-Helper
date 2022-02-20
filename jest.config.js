53b8c382f06e68000d244cc10059a37005104dfa
Ryan <betrix303@gmail.com>
2022-02-20 13:48:24 -0500

    Rename test directory

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset: 'ts-jest/presets/js-with-ts',
-    verbose: true,
     coveragePathIgnorePatterns: ['/node_modules/'],
-    moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src']
+    moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
+    testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
 }
