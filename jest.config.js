5878a675b3852d0d8b3c27c2b8b47eeb25791a53
Ryan <betrix303@gmail.com>
2022-03-07 15:06:47 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-    preset: 'ts-jest/presets/js-with-ts',
+    preset: 'ts-jest/presets/default',
     coveragePathIgnorePatterns: ['/node_modules/'],
     moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/src'],
     testMatch: ['**/test/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
