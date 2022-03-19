de8fcf8a89abc2b1b4a577f1d0951f70b04d95f7
Ryan <betrix303@gmail.com>
2022-03-19 01:25:46 -0400

    Fix rate limiter and adjust cooldown time

--- src/guards/rate-limiter.ts
+++ src/guards/rate-limiter.ts
@@ -5,8 +5,8 @@ import { round, translation } from '../lib'
 
 // One the first message from a user, they have a set amount of commands they can issue within a time period before they are on a cooldown
 
-const COOLDOWN_LENGTH = 10 * 1000
-const MAX_REQUESTS = 5
+const COOLDOWN_LENGTH = 5 * 1000
+const MAX_REQUESTS = 2
 
 interface LimiterValue {
     requestsLeft: number
@@ -17,46 +17,43 @@ const limiter = new Map<string, LimiterValue>()
 
 const isOwner = (interaction: Interaction) => interaction.guild?.ownerId === interaction.user.id
 
-export const RateLimiterGuard: GuardFunction<CommandInteraction, GuardData> = async (
-    interaction,
-    client,
-    next,
-    guardData
-) => {
-    if (!interaction.isApplicationCommand || isOwner(interaction)) {
-        // Only rate limit commands and people who aren't owners of the server
-        await next()
-        return
-    }
+export const RateLimiterGuard: GuardFunction<CommandInteraction, GuardData> = async (arg, client, next, guardData) => {
+    const interaction = arg instanceof Array ? arg[0] : arg
 
-    const uid = interaction.user.id
+    if (interaction instanceof CommandInteraction && !isOwner(interaction)) {
+        const uid = interaction.user.id
 
-    if (limiter.has(uid)) {
-        const userData = limiter.get(uid) as LimiterValue
+        if (limiter.has(uid)) {
+            const userData = limiter.get(uid) as LimiterValue
 
-        if (userData.requestsLeft > 0) {
-            limiter.set(uid, { requestsLeft: userData.requestsLeft - 1, firstMessage: userData.firstMessage })
+            if (userData.requestsLeft > 0) {
+                limiter.set(uid, { requestsLeft: userData.requestsLeft - 1, firstMessage: userData.firstMessage })
+
+                next()
+                return
+            } else {
+                const cooldownTimeLeft = (userData.firstMessage + COOLDOWN_LENGTH - Date.now()) / 1000
+
+                // rate limiting
+                interaction.reply({
+                    content: translation(guardData.serverData.Language)(
+                        '**Cooldown**: Please wait {0} seconds before making another request',
+                        round(cooldownTimeLeft, '00')
+                    ),
+                    ephemeral: true
+                })
+            }
+        } else {
+            limiter.set(uid, { requestsLeft: MAX_REQUESTS, firstMessage: Date.now() })
+
+            setTimeout(() => {
+                limiter.delete(uid)
+            }, COOLDOWN_LENGTH)
 
             await next()
-            return
-        } else {
-            const cooldownTimeLeft = (userData.firstMessage + COOLDOWN_LENGTH - Date.now()) / 1000
-
-            interaction.reply({
-                content: translation(guardData.serverData.Language)(
-                    '**Cooldown**: Please wait {0} seconds before making another request',
-                    round(cooldownTimeLeft, '00')
-                ),
-                ephemeral: true
-            })
         }
     } else {
-        limiter.set(uid, { requestsLeft: MAX_REQUESTS, firstMessage: Date.now() })
-
-        setTimeout(() => {
-            limiter.delete(uid)
-        }, COOLDOWN_LENGTH)
-
-        await next()
+        // Only rate limit commands and people who aren't owners of the server
+        next()
     }
 }
