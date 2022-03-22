aad491ad286bc8c73654aaf5b9e21f84d5a2dfb9
Ryan <betrix303@gmail.com>
2022-03-22 15:58:30 -0400

    Fix mongoDB integration

--- src/models/server-model.ts
+++ src/models/server-model.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose'
 
 const serverSchema = new Schema({
     ServerID: { type: String, required: true, unique: true },
-    ChannelLock: { type: String, required: true, default: '' },
+    ChannelLock: { type: String, required: false, default: '' },
     Language: { type: String, required: true, default: 'en' },
     Cooldown: { type: Number, required: true, default: 3 }
 })
