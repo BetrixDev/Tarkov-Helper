6a0ebfded463dd8c1753483e24bb96e63ae66433
Ryan <betrix303@gmail.com>
2022-04-26 21:55:13 -0400

    Make random() test more robust

--- test/helpers/utils/Math.test.ts
+++ test/helpers/utils/Math.test.ts
@@ -8,13 +8,15 @@ describe('Math Tests', () => {
     })
 
     it('should generate a random number', () => {
-        const rand1 = random(1, 10)
-        const rand2 = random(50, 1000)
-        const rand3 = random(25, 250)
+        for (let i = 0; i < 1000; i++) {
+            const rand1 = random(1, 10)
+            const rand2 = random(50, 1000)
+            const rand3 = random(25, 250)
 
-        expect(rand1 >= 1 && rand1 <= 10).toBeTruthy()
-        expect(rand2 >= 50 && rand2 <= 1000).toBeTruthy()
-        expect(rand3 >= 25 && rand3 <= 250).toBeTruthy()
+            expect(rand1 >= 1 && rand1 <= 10).toBeTruthy()
+            expect(rand2 >= 50 && rand2 <= 1000).toBeTruthy()
+            expect(rand3 >= 25 && rand3 <= 250).toBeTruthy()
+        }
     })
 
     it('should clamp numbers', () => {
