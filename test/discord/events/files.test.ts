c25cc2b27fbaa5118f5bf901e406e3529ae3400f
Ryan <betrix303@gmail.com>
2022-08-05 00:17:37 -0400

    remove old test files

--- test/discord/events/files.test.ts
+++
@@ -1,7 +0,0 @@
-import { readJson } from "../../../src/lib/util/files";
-
-describe("Files", function () {
-    it("should read from a json file", () => {
-        expect(readJson("package.json")).toBeDefined();
-    });
-});
