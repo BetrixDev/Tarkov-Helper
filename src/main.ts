a4392963a6886f9601e9158c4df7fe7dfbbbe915
Ryan <betrix303@gmail.com>
2022-08-06 22:17:24 -0400

    add restocks command

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata";
-import { Client, DIService } from "discordx";
-import {IntentsBitField } from "discord.js";
+import { Client, DIService, tsyringeDependencyRegistryEngine } from "discordx";
+import { IntentsBitField } from "discord.js";
 import { importx } from "@discordx/importer";
 import dotenv from "dotenv";
-import { container } from "tsyringe";
+import { container, Lifecycle } from "tsyringe";
 import { config } from "./config";
 import { TarkovDataService } from "./services/TarkovDataService";
 import { ItemSearchEngine } from "./lib/search_engines/ItemSearchEngine";
@@ -14,7 +14,7 @@ const isDev = config.process.isDev;
 const isTest = config.process.isTest;
 
 // Dependency Injection Setup
-// DIService.container = container;
+DIService.engine = tsyringeDependencyRegistryEngine.setInjector(container);
 
 export const client = new Client({
     botGuilds: isDev ? [(client) => client.guilds.cache.map((guild) => guild.id)] : undefined,
@@ -33,11 +33,13 @@ client.once("ready", async () => {
         // useful when moving to global commands from guild commands
         await client.clearApplicationCommands(...client.guilds.cache.map((g) => g.id));
     }
-    
+
     console.log("Tarkov Helper started");
 });
 
 export async function main(): Promise<void> {
+    container.register("TarkovDataService", { useClass: TarkovDataService }, { lifecycle: Lifecycle.Singleton });
+
     // Cache initial data
     const dataService = container.resolve(TarkovDataService);
 
