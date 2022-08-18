cc272d0d171a387a8a803eae10f992af75ee3e2f
Ryan <betrix303@gmail.com>
2022-08-18 14:31:18 -0400

    remove if statements from logger

--- src/logger.ts
+++ src/logger.ts
@@ -4,24 +4,20 @@ const getTimeStamp = () => {
     return dayjs().format("(MM/DD/YYYY) HH:mm:ss");
 };
 
-const info = (namespace: string, message: string, object?: any) => {
-    if (object) console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
-    else console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+const info = (namespace: string, message: string, object?: unknown) => {
+    console.log(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object ? object : "");
 };
 
-const warn = (namespace: string, message: string, object?: any) => {
-    if (object) console.log(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object);
-    else console.log(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
+const warn = (namespace: string, message: string, object?: unknown) => {
+    console.log(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object ? object : "");
 };
 
-const error = (namespace: string, message: string, object?: any) => {
-    if (object) console.log(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
-    else console.log(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+const error = (namespace: string, message: string, object?: unknown) => {
+    console.log(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object ? object : "");
 };
 
-const debug = (namespace: string, message: string, object?: any) => {
-    if (object) console.log(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object);
-    else console.log(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
+const debug = (namespace: string, message: string, object?: unknown) => {
+    console.log(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object ? object : "");
 };
 
 export default { info, warn, error, debug };
