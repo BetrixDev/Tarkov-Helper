b74b6275a7f43e9343f1096d7feab98699c90911
Ryan <betrix303@gmail.com>
2022-12-05 15:47:54 -0500

    fix env type check

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,9 @@ dotenv.config();
 
 const ENVIROMENT = z.object({
     /** Internal */
-    NODE_ENV: z.union([z.literal("production"), z.literal("development"), z.literal("testing")]),
+    NODE_ENV: z
+        .union([z.literal("production"), z.literal("development"), z.literal("testing"), z.string()])
+        .default(""),
     /** Client token for logging in your bot */
     BOT_TOKEN: z.string(),
     /** tarkov-changes.com token for grabbing up-to-date item properties */
@@ -19,7 +21,8 @@ const ENVIROMENT = z.object({
 const parsed = ENVIROMENT.safeParse(process.env);
 
 if (!parsed.success) {
-    throw new Error(`Enviroment variables appear to be malformed, ${parsed.error.errors.join("\n")}`);
+    parsed.error.errors.forEach((e) => console.error(e.code, e.path));
+    throw new Error(`Enviroment variables appear to be malformed`);
 }
 
 export const env = parsed.data;
