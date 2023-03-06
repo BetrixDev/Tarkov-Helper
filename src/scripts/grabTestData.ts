5a1d3cff3591127ee2dcf5d4b76a2109088988cb
Ryan <betrix303@gmail.com>
2023-03-05 21:54:29 -0500

    Update grabTestData.ts

--- src/scripts/grabTestData.ts
+++ src/scripts/grabTestData.ts
@@ -4,17 +4,17 @@ import { container } from "tsyringe";
 import { TarkovDataService } from "../services/TarkovDataService";
 
 async function main() {
-    if (existsSync("./runtime_data/")) {
-        rmSync("./runtime_data/", { recursive: true, force: true });
+    if (existsSync("./data/")) {
+        rmSync("./data/", { recursive: true, force: true });
     }
 
-    mkdirSync("./runtime_data/");
+    mkdirSync("./data/");
 
     const dataService = container.resolve(TarkovDataService);
     const data = await dataService.fetchTestData();
 
     data.forEach((e) => {
-        writeFileSync(`./runtime_data/${e.name.replaceAll("/", "_")}.json`, JSON.stringify(e.data, null, 4));
+        writeFileSync(`./data/${e.name.replaceAll("/", "_")}.json`, JSON.stringify(e.data, null, 4));
     });
 }
 
