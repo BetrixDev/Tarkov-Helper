b14a170fc4f2df48519b9cdad2fcd218c4d1367f
Ryan <betrix303@gmail.com>
2023-04-27 15:10:21 -0400

    Clean Slate

--- src/lib/files.ts
+++
@@ -1,5 +0,0 @@
-import { PathLike, readFileSync } from "fs";
-
-export function readJson<T>(path: PathLike | string): T {
-    return JSON.parse(readFileSync(path).toString()) as T;
-}
--- src/typings/common.d.ts
+++
@@ -1,2 +0,0 @@
-/** Fully supported languages in Tarkov Helper */
-export type LanguageCode = "en" | "es";
