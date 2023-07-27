8387939f5a30f6b7f7a9e176b0085cce89509a7b
Ryan <betrix303@gmail.com>
2023-07-27 13:24:58 -0400

    Update currency.ts

--- packages/common/src/currency.ts
+++ packages/common/src/currency.ts
@@ -18,11 +18,10 @@ export const CURRENCIES = [
   },
 ] as const;
 
-export const CURRENCY_IDS: Record<Currency, string> = {
-  rub: "5449016a4bdc2d6f028b456f",
-  usd: "5696686a4bdc2da3298b456a",
-  eur: "569668774bdc2da2298b4568",
-};
+export const CURRENCY_IDS = CURRENCIES.reduce(
+  (p, c) => ({ ...p, [c.id]: c.gameId }),
+  {}
+) as Record<Currency, string>;
 
 export const currencySchema = z.union([
   z.literal("rub"),
